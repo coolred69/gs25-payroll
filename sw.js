4db6ca6123a844ee98c8f830b44725c46b735d89
coolred69 <156668862+coolred69@users.noreply.github.com>
2026-03-15 17:53:18 +0900

    update

--- sw.js
+++ sw.js
@@ -1,36 +1,21 @@
-const CACHE_NAME = 'gs25-pwa-v13';
-const ASSETS = [
-  './', './index.html', './manifest.json',
-  './icons/icon-72.png','./icons/icon-96.png','./icons/icon-128.png',
-  './icons/icon-144.png','./icons/icon-152.png','./icons/icon-192.png',
-  './icons/icon-384.png','./icons/icon-512.png'
-];
+const CACHE_NAME = 'gs25-payroll-v13';
+const URLS_TO_CACHE = ['./', './index.html', './manifest.json'];
 
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(URLS_TO_CACHE)).then(() => self.skipWaiting()));
 });
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      if (cached) return cached;
-      return fetch(event.request).then(res => {
-        if (res && res.status === 200) {
-          const clone = res.clone();
-          caches.open(CACHE_NAME).then(c => c.put(event.request, clone));
-        }
-        return res;
-      });
-    }).catch(() => caches.match('./index.html'))
-  );
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(caches.match(e.request).then(cached => {
+    if (cached) return cached;
+    return fetch(e.request).then(res => {
+      if (!res || res.status !== 200) return res;
+      const clone = res.clone();
+      caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match('./index.html'));
+  }));
 });
